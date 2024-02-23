0a9f0fcbb7c8f5594fd82c31e9605d204ec677b8
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-02-23 19:25:06 +0900

    업데이트 안됨

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './util/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
